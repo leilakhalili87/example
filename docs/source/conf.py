20355d29130048c2efdb81db9e267cb176b73266
lkhalil <lkhalil@ncsu.edu>
2020-05-08 17:55:48 -0400

    try

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,9 +17,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('..'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- General configuration ------------------------------------------------
@@ -33,16 +33,16 @@
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.githubpages',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.mathjax',
+    # 'sphinx.ext.autosummary',
+    # 'sphinx.ext.githubpages',
+    # 'sphinx.ext.intersphinx',
+    # 'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'IPython.sphinxext.ipython_directive',
-    'IPython.sphinxext.ipython_console_highlighting',
-    'matplotlib.sphinxext.plot_directive',
-    'numpydoc',
-    'sphinx_copybutton',
+    # 'IPython.sphinxext.ipython_directive',
+    # 'IPython.sphinxext.ipython_console_highlighting',
+    # 'matplotlib.sphinxext.plot_directive',
+    # 'numpydoc',
+    # 'sphinx_copybutton',
 ]
 
 # Configuration options for plot_directive. See:

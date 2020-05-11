3895e6fd56734351236b8b19c4a80e05b289491a
lkhalil <lkhalil@ncsu.edu>
2020-05-11 17:09:30 -0400

    Add

--- docs/conf.py
+++ docs/conf.py
@@ -55,7 +55,7 @@ plot_html_show_formats = False
 autosummary_generate = True
 numpydoc_show_class_members = False
 
-nbsphinx_allow_errors = True
+# nbsphinx_allow_errors = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -64,8 +64,8 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-# source_suffix = '.rst'
-source_suffix = ['.rst', '.ipynb']
+source_suffix = '.rst'
+# source_suffix = ['.rst', '.ipynb']
 
 # The master toctree document.
 master_doc = 'index'
